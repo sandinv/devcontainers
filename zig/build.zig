f2b4529736fde781392278a19647782eafb799cb
Victor Sandin <victor.sandin@tabby.ai>
2025-04-08 12:47:44 +0200

    add sqlite3 example

---
+++ zig/build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "zig_labs",
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    exe.linkSystemLibrary("sqlite3");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
--- zig/main.zig
+++ zig/main.zig
@@ -1,6 +1,169 @@
 const std = @import("std");
+const c = @cImport({
+    @cInclude("sqlite3.h");
+});
 
+const print = std.debug.print;
 
-pub fn main() {
-	std.debug.print("Hello World", .{});
+const DB = struct {
+    db: *c.sqlite3,
+
+    fn init(db: *c.sqlite3) DB {
+        return .{
+            .db = db,
+        };
+    }
+
+    fn deinit(self: DB) void {
+        _ = c.sqlite3_close(self.db);
+    }
+
+    fn execute(self: DB, query: [:0]const u8) !void {
+        var errmsg: [*c]u8 = undefined;
+
+        if (c.sqlite3_exec(self.db, query, null, null, &errmsg) != c.SQLITE_OK) {
+            defer c.sqlite3_free(errmsg);
+            print("Exec query failed: {s}\n", .{errmsg});
+            return error.execError;
+        }
+
+        return;
+    }
+
+    fn queryTable(self: DB) !void {
+        const stmt = blk: {
+            var stmt: ?*c.sqlite3_stmt = undefined;
+
+            const query =
+                \\SELECT
+                \\   c.name,
+                \\   cc.name
+                \\FROM
+                \\  cats c
+                \\  INNER JOIN cat_colors cc ON cc.id = c.color_id;
+                \\
+            ;
+            if (c.SQLITE_OK != c.sqlite3_prepare_v2(self.db, query, query.len + 1, &stmt, null)) {
+                print("Can't create prepare statement: {s}", .{c.sqlite3_errmsg(self.db)});
+                return error.prepareStmt;
+            }
+            break :blk stmt.?;
+        };
+        defer _ = c.sqlite3_finalize(stmt);
+        var rc = c.sqlite3_step(stmt);
+        while (rc == c.SQLITE_ROW) {
+            const cat_name = c.sqlite3_column_text(stmt, 0);
+            const color_name = c.sqlite3_column_text(stmt, 1);
+            print("Cat {s} is in {s} color\n", .{ cat_name, color_name });
+            rc = c.sqlite3_step(stmt);
+        }
+
+        if (rc != c.SQLITE_DONE) {
+            print("Step query failed: {s}\n", .{c.sqlite3_errmsg(self.db)});
+            return error.stepQuery;
+        }
+    }
+
+    fn insertTable(self: DB) !void {
+        const insert_color_stmt = blk: {
+            var stmt: ?*c.sqlite3_stmt = undefined;
+
+            const query = "INSERT INTO cat_colors (name) values (?1)";
+            if (c.SQLITE_OK != c.sqlite3_prepare_v2(self.db, query, query.len + 1, &stmt, null)) {
+                print("Can't prepare statement: {s}\n", .{c.sqlite3_errmsg(self.db)});
+                return error.prepareStmt;
+            }
+            break :blk stmt.?;
+        };
+        defer _ = c.sqlite3_finalize(insert_color_stmt);
+
+        const insert_cat_stmt = blk: {
+            var stmt: ?*c.sqlite3_stmt = undefined;
+            const query = "INSERT INTO cats (name, color_id) values (?1, ?2)";
+            if (c.SQLITE_OK != c.sqlite3_prepare_v2(self.db, query, query.len + 1, &stmt, null)) {
+                print("Can't prepare statement: {s}\n", .{c.sqlite3_errmsg(self.db)});
+                return error.prepareStmt;
+            }
+            break :blk stmt.?;
+        };
+        defer _ = c.sqlite3_finalize(insert_cat_stmt);
+
+        const cat_colors = .{
+            .{
+                "Blue",
+                .{
+                    "Tiger",
+                    "Sammy",
+                },
+            },
+            .{
+                "Black",
+                .{
+                    "Oreo",
+                    "Biscuit",
+                },
+            },
+        };
+        inline for (cat_colors) |row| {
+            const color = row.@"0";
+            const cat_names = row.@"1";
+            if (c.SQLITE_OK != c.sqlite3_bind_text(insert_color_stmt, 1, color, color.len + 1, c.SQLITE_STATIC)) {
+                print("Can't bind text: {s}\n", .{c.sqlite3_errmsg(self.db)});
+                return error.bindText;
+            }
+            if (c.SQLITE_DONE != c.sqlite3_step(insert_color_stmt)) {
+                print("Can't step color stmt: {s}", .{c.sqlite3_errmsg(self.db)});
+                return error.step;
+            }
+            _ = c.sqlite3_reset(insert_color_stmt);
+            const last_id = c.sqlite3_last_insert_rowid(self.db);
+            inline for (cat_names) |cat_name| {
+                if (c.SQLITE_OK != c.sqlite3_bind_text(insert_cat_stmt, 1, cat_name, cat_name.len + 1, c.SQLITE_STATIC)) {
+                    print("Can't bind text: {s}\n", .{c.sqlite3_errmsg(self.db)});
+                    return error.bindText;
+                }
+                if (c.SQLITE_OK != c.sqlite3_bind_int64(insert_cat_stmt, 2, last_id)) {
+                    print("Can't bind int64 {s}\n", .{c.sqlite3_errmsg(self.db)});
+                    return error.bindInt64;
+                }
+                if (c.SQLITE_DONE != c.sqlite3_step(insert_cat_stmt)) {
+                    print("Can't step cat stmt: {s}", .{c.sqlite3_errmsg(self.db)});
+                    return error.step;
+                }
+                _ = c.sqlite3_reset(insert_cat_stmt);
+            }
+        }
+
+        return;
+    }
+};
+
+pub fn main() !void {
+    const version = c.sqlite3_libversion();
+    print("libsqlite3 version is {s}\n", .{version});
+
+    var c_db: ?*c.sqlite3 = undefined;
+    if (c.SQLITE_OK != c.sqlite3_open(":memory:", &c_db)) {
+        //if (c.SQLITE_OK != c.sqlite3_open("/tmp/test.sqlite3", &c_db)) {
+        print("Can't open database: {s}\n", .{c.sqlite3_errmsg(c_db)});
+        return;
+    }
+
+    const db = DB.init(c_db.?);
+    defer db.deinit();
+
+    try db.execute(
+        \\ create table if not exists cat_colors (
+        \\      id integer primary key,
+        \\          name text not null unique
+        \\ );
+        \\ create table if not exists cats (
+        \\    id integer primary key,
+        \\    name text not null,
+        \\    color_id integer not null references cat_colors(id)
+        \\ );
+    );
+
+    try db.insertTable();
+    try db.queryTable();
 }
