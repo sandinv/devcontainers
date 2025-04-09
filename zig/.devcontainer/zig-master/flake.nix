182bab6cf5e3cc57d4fdaa992bee438be680d41b
Victor Sandin <victor.sandin@tabby.ai>
2025-04-09 13:10:43 +0200

    Add zls to devenv

--- zig/.devcontainer/zig-master/flake.nix
+++ zig/.devcontainer/zig-master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Zig from master";
+  description = "Zig and ZLS from master";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -14,8 +14,41 @@
           inherit system;
           overlays = [ zig-overlay.overlays.default ];
         };
+        zig = pkgs.zigpkgs.master;
+
+        zls-src = pkgs.fetchFromGitHub {
+          owner = "zigtools";
+          repo = "zls";
+          rev = "99eaaf61abf3dc35c6c6c49e3459b632eeda3dfe";
+          sha256 = "sha256-siRiMBlRfHySRhwzenUp93oPm1l0ZgJ9sJ8dd/kXITk=";
+          # You can pin a commit and add sha256 for reproducibility
+          # rev = "somecommit";
+          # sha256 = "sha256-value";
+        };
       in {
-        packages.zig-master = pkgs.zigpkgs.master;
-        defaultPackage = pkgs.zigpkgs.master;
+        packages = {
+          zig-master = zig;
+
+          zls = pkgs.stdenv.mkDerivation {
+            pname = "zls";
+            version = "master";
+
+            src = zls-src;
+
+            nativeBuildInputs = [ zig ];
+
+            buildPhase = ''
+              export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+              zig build -Doptimize=ReleaseFast
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp zig-out/bin/zls $out/bin/
+            '';
+          };
+
+          default = pkgs.zigpkgs.master;
+        };
       });
 }
